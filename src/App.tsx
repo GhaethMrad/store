de83534ca5ba569349884387f10c8cae8daaa346
Ghaeth Mrad <mradgaeth40@gmail.com>
2025-06-23 14:31:15 -0400

    update

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import "./App.css";
 import {
   Routes,
   Route,
-  Navigate,
 } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import NavigationBar from "./components/NavigationBar";
